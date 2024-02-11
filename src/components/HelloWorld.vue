b1f5241219a8a1f738a8286032b7108662375c10
Jamessmith <jamessmith@clogic.xyz>
2024-02-11 12:09:58 +0000

    feat: 安装依赖包，并启动项目

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,13 +6,17 @@ defineProps({
 })
 
 const count = ref(0)
+
+function addCount() {
+  count.value++;
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="addCount">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
