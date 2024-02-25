681ecc4dff035f22a3b66255b1716605ced35301
jamessmith <jamessmith@clogic.xyz>
2024-02-25 03:10:05 +0000

    feat: 使用“vite-plugin-mdx”编译 mdx，失败：[vite-plugin-mdx] "react" must be installed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from "@vitejs/plugin-vue-jsx"
+import mdx from 'vite-plugin-mdx'
+console.log(mdx.default())
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx(), mdx.default({ jsxImportSource: 'vue'})],
 })
